ebc212c13a9501dcd8143a9be62a06e288fda41c
Chris <oodachris@hotmail.co.uk>
2021-11-23 21:41:10 +0000

    Create permissions.sh

---
+++ permissions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /home/pi/roamerpi
+echo "Setting script permissions"
+chmod 755 pull.sh
+chmod 755 startnjpeg.sh
+chmod 755 ytchecker.sh
+chmod 755 ffmpeg.sh
+chmod 755 startgps.sh
+chmod 755 startup.sh
+chmod 755 kill.sh
