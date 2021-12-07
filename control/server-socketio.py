2417aca0316c0524a515949f2ecd20af53bab031
Chris <oodachris@hotmail.co.uk>
2021-12-07 18:56:59 +0000

    Update server-socketio.py

--- control/server-socketio.py
+++ control/server-socketio.py
@@ -234,8 +234,8 @@ def task1():
 		if (int(time.time())>=int(lasttime+2)):
 			print("timeout")
 			stp()
-	except e:
-		print("closing task1 " + e)
+	except BaseException as error:
+	    print('Closing task1 An exception occurred: {}'.format(error))
 
 #def taskstart():
 #    for t in tasks:
