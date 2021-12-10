71cf8b752765bec4637cd65ac79b2fc79b0f5177
Chris <oodachris@hotmail.co.uk>
2021-12-10 21:15:16 +0000

    Update server-socketio.py

--- control/server-socketio.py
+++ control/server-socketio.py
@@ -87,8 +87,8 @@ async def temeletry():
 			feedback = ser.read_all()
 			#print(feedback)
 			if feedback:
-				print("Anticipated " + struct.unpack('<H', bytes.fromhex('ABCD')[::-1]))
-				print("startbytes: " + struct.unpack('<H', feedback[0:1]))
+				if feedback[0:1]==bytes.fromhex('ABCD')[::-1]:
+					print("same")
 				cmd1, cmd2, speedR_meas, speedL_meas, batVoltage, boardTemp, cmdLed = struct.unpack('<hhhhhhH', feedback[2:16])
 				print(f'cmd1: {cmd1}, cmd2: {cmd2}, speedR_meas: {speedR_meas}, speedL_meas: {speedL_meas}, batVoltage: {batVoltage}, boardTemp: {boardTemp}, cmdLed: {cmdLed}')	
 				await sio.emit('telemetry', {"cmd1": cmd1, "cmd2": cmd2, "speedR_meas": speedR_meas, "speedL_meas": speedL_meas, "batVoltage": batVoltage, "boardTemp": boardTemp, "cmdLed": cmdLed})
