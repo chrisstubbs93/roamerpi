4445c26c24660a84e7b4b751f6a19a6b17cc043b
Chris <oodachris@hotmail.co.uk>
2022-05-19 16:57:54 +0100

    Update server-socketio.py

--- control/server-socketio.py
+++ control/server-socketio.py
@@ -353,8 +353,8 @@ async def bodyControl():
 					await handleBump(bodyControlData)
 
 				if "BUMP" not in bodyControlData and "SONAR" not in bodyControlData: # neither Bump or SONAR so we'll treat this as GPS data
-					print("About to handle: " + rawNavSparkData)
-					await handleGps(str(rawNavSparkData.decode('utf-8'))) #send raw NMEA to GPS parser
+					print("About to handle: " + rawNavSparkData.decode('utf-8'))
+					await handleGps(rawNavSparkData.decode('utf-8')) #send raw NMEA to GPS parser
 
 async def lightingControl():
 	global leftIndicate
